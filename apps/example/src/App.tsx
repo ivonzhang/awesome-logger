58ed6feccc8ae325ba8af225b9ba412d25c5743a
ivonzhang <zhangivon@gmail.com>
2025-03-28 16:51:27 +0800

    feat: show how to use plugin-sls

--- apps/example/src/App.tsx
+++ apps/example/src/App.tsx
@@ -3,18 +3,42 @@ import { client } from './utils/init';
 import './App.css'
 
 function App() {
-  const handleClick = () => {
-    client.info('click', {
-      message: 'click button',
-    });
+  const handleClick = (type: 'info' | 'error' | 'warn') => {
+    switch (type) {
+      case 'info':
+        client.info('click', {
+          message: 'click button',
+        });
+        break;
+      case 'error':
+        client.error('error', {
+          err: new Error('error message'),
+        });
+        break;
+      case 'warn':
+        client.warn('warn', {
+          message: 'warn message',
+        });
+        break;
+      default:
+        client.info('click', {
+          message: 'click button',
+        });
+  }
   }
 
   return (
     <>
       <h1>awesome logger demo</h1>
       <div className="card">
-        <button onClick={handleClick}>
-          send log
+        <button onClick={() => handleClick('info')}>
+          send info log
+        </button>
+        <button onClick={() => handleClick('error')}>
+          send error log
+        </button>
+        <button onClick={() => handleClick('warn')}>
+          send warn log
         </button>
         <p>
           Please open the <code>DevTool</code> to see the request log
--- apps/example/src/utils/init.ts
+++ apps/example/src/utils/init.ts
@@ -2,7 +2,7 @@ import Client from '@awesome-logger/client';
 import { Logger } from '@awesome-logger/core';
 import MyCustomLogPlugin from '../plugin';
 
-// import { SLSLogPlugin } from '@awesome-logger/plugin-sls';
+import { SLSLogPlugin } from '@awesome-logger/plugin-sls';
 
 // create logger 
 const logger = new Logger({
@@ -14,9 +14,18 @@ const logger = new Logger({
 // Configure basic fields
 const client = new Client(logger);
 
+// sls plugin
+const slsPlugin = new SLSLogPlugin({
+  host: 'cn-hangzhou.log.aliyuncs.com', // 公网域名
+  project: 'awesome-logger', // log project
+  logstore: 'plugin-sls-logstore', // logstore
+  count: 20, // 发送条数阈值
+  time: 3, // 发送时间阈值
+});
+client.usePlugin(slsPlugin);
+
 // custom plugin
 const customPlugin = new MyCustomLogPlugin(null);
-
 client.usePlugin(customPlugin);
 
 export { client };
